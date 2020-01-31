e9cf371a1511839ae55f36342fb43f5d6d3e578a
Parsa Nooralinejad <parsanooralinejad@yahoo.com>
2020-01-31 16:28:56 +0330

    This is what we had to do

--- main.py
+++ main.py
@@ -3,11 +3,31 @@ import warnings
 
 warnings.simplefilter("ignore")
 
-print('enter file address: ')
-file_name = input()
+file_name = 'top_words.csv'
 File = open(file_name, 'r')
-persian_words = File.readlines()
+All_file = File.readlines()
 File.close()
 
-topic_labeling = TopicLabeling(persian_words)
-print(topic_labeling.assign_label())
+All_file = All_file[1:]
+labels = {}
+for line in All_file:
+	L = line.split('\t')
+	if L[1] not in labels.keys():
+		labels[L[1]] = []
+	labels[L[1]].append(L[2])
+
+assigned = {}
+
+File = open('assigned labels','w', encoding="utf-8")
+
+cnt = 0
+for k in labels.keys():
+	persian_words = labels[k]
+	topic_labeling = TopicLabeling(persian_words)
+	assigned[k] = topic_labeling.assign_label()
+	File.write(str(k) + ',' + str(assigned[k]) + '\n')
+	cnt += 1
+	if cnt == 2:
+		break
+
+File.close()
