90c704a3f3b25c996cbeae40d2f2aff6f49f3f7b
Parsa Nooralinejad <parsanooralinejad@yahoo.com>
2020-02-19 10:39:39 +0330

    testing one topic

--- testing_NLP.py
+++ testing_NLP.py
@@ -3,16 +3,13 @@ import warnings
 
 warnings.simplefilter("ignore")
 
-file_name = './assets/word_list_farsi_test'
-File = open(file_name, 'r')
+file_name = './assets/word_list_farsi_test_4'
+File = open(file_name, 'r', encoding = "utf-8")
 All_file = File.readlines()
 File.close()
 
 for i in range(0,len(All_file)):
 	All_file[i] = All_file[i][:-1]
 
-print(All_file)
-
-input()
 topic_labeling = TopicLabeling(All_file)
 print(topic_labeling.assign_label())
